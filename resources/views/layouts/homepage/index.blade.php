96e507b57fad0ec75087bd0c762474155112540d
Farhan <farhan18apr02@gmail.com>
2022-11-03 10:09:00 +0700

    Fixing Production

--- resources/views/layouts/homepage/index.blade.php
+++ resources/views/layouts/homepage/index.blade.php
@@ -14,11 +14,11 @@
         rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link href="{{ asset('/assets/lib/animate/animate.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/assets/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('/assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/assets/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('/assets/css/style.css') }}" rel="stylesheet">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     @livewireStyles
@@ -39,14 +39,14 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{ asset('/assets/lib/wow/wow.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/easing/easing.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/waypoints/waypoints.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/counterup/counterup.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
-    <script src="{{ asset('/assets/lib/lightbox/js/lightbox.min.js') }}"></script>
-    <script src="{{ asset('/assets/js/main.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/wow/wow.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/counterup/counterup.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/isotope/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/lib/lightbox/js/lightbox.min.js') }}"></script>
+    <script src="{{ secure_asset('/assets/js/main.js') }}"></script>
 
 
     @stack('javascript')
