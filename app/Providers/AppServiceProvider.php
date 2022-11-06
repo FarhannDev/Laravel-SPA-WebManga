faa4d0724f5af024d20e97761cd8de3ce2821476
Farhan <farhan18apr02@gmail.com>
2022-11-06 17:42:50 +0700

    Fixing conflict Production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use \Illuminate\Support\Facades\URL;
-<<<<<<< HEAD
 use Illuminate\Pagination\Paginator;
-=======
->>>>>>> 7d62a52dd05649d0d8c46880ba00e06e9dd19a72
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,9 +28,6 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
-<<<<<<< HEAD
         Paginator::useBootstrap();
-=======
->>>>>>> 7d62a52dd05649d0d8c46880ba00e06e9dd19a72
     }
 }
