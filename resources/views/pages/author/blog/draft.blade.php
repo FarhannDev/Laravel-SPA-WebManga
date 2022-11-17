14f178cfee96a220847f57b1b8df9ddc4e275502
Farhan <101630148+frhan18@users.noreply.github.com>
2022-11-17 09:17:56 +0700

    Final Dashboard Author

--- resources/views/pages/author/blog/draft.blade.php
+++ resources/views/pages/author/blog/draft.blade.php
@@ -25,17 +25,7 @@
                             </div>
                         </div>
                     @endif
-
-                    <div class="dashboard-inner border-bottom mb-3">
-                        <div class="dashboard-inner__text px-3 mx-2 pt-2 mb-2">
-                            <div class="d-flex justify-content-arroud align-items-center">
-                                <span class="fas fa-2x fa-bookmark mr-2"></span>
-                                <h4 class="text-dark text-capitalize pt-2">Kelola semua postingan tersimpan</h4>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="dashboard-inner__item px-3 mb-3">
+                    <div class="dashboard-inner__item px-3 mb-3 p-3">
                         <div class="dashboard-item__list">
                             <div class="row justify-content-arround align-items-center">
                                 <div class="col">
@@ -58,7 +48,7 @@
                                                                 {{ $loop->iteration }}
                                                             </td>
                                                             <td class="align-middle"><a
-                                                                    href="{{ route('manageBlogShow', $data->blog_slug) }}"
+                                                                    href="{{ route('authorBlogShow', $data->blog_slug) }}"
                                                                     class="btn btn-link text-dark">{{ $data->blog_name }}</a>
                                                             </td>
                                                             <td class="align-middle">
@@ -74,8 +64,7 @@
                                                                     @csrf
                                                                     @method('PUT')
                                                                     <button type="submit"
-                                                                        class="btn btn-success btn-sm">Publish
-                                                                        To Post </span>
+                                                                        class="btn btn-success btn-md">Terbitkan</span>
                                                                 </form>
                                                             </td>
                                                         </tr>
--- resources/views/pages/author/blog/publish.blade.php
+++ resources/views/pages/author/blog/publish.blade.php
@@ -25,17 +25,7 @@
                             </div>
                         </div>
                     @endif
-
-                    <div class="dashboard-inner border-bottom mb-3">
-                        <div class="dashboard-inner__text px-3 mx-2 pt-2 mb-2">
-                            <div class="d-flex justify-content-arroud align-items-center">
-                                <span class="fas fa-2x fa-globe mr-2"></span>
-                                <h4 class="text-dark text-capitalize pt-2">Kelola semua postingan publish</h4>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="dashboard-inner__item px-3 mb-3">
+                    <div class="dashboard-inner__item px-3 mb-3 p-3">
                         <div class="dashboard-item__list">
                             <div class="row justify-content-arround align-items-center">
                                 <div class="col">
@@ -58,7 +48,7 @@
                                                                 {{ $loop->iteration }}
                                                             </td>
                                                             <td class="align-middle"><a
-                                                                    href="{{ route('manageBlogShow', $data->blog_slug) }}"
+                                                                    href="{{ route('authorBlogShow', $data->blog_slug) }}"
                                                                     class="btn btn-link text-dark">{{ $data->blog_name }}</a>
                                                             </td>
                                                             <td class="align-middle">
@@ -74,8 +64,7 @@
                                                                     @csrf
                                                                     @method('PUT')
                                                                     <button type="submit"
-                                                                        class="btn btn-danger btn-sm">Draft
-                                                                        To Post </span>
+                                                                        class="btn btn-danger btn-md">Simpan Ke Draft</span>
                                                                 </form>
                                                             </td>
                                                         </tr>
